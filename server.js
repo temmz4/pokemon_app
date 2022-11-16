2efbd61413b0b24cc3219aaadb59bcfe6ffcab6b
toby emelle <temmz4@users.noreply.github.com>
2022-11-16 18:15:19 -0500

    stuck on set up your index view part

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = express()
 const PORT = 3000
 const pokemon = require('./models/pokemon')
 
+app.set("view engine", "jsx")
+app.engine("jsx", reactViews.createEngine())
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!');
 })
 
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon)
+    // res.send(pokemon)
+    res.render('pokemon/Index', {pokemon})
 })
 
 app.listen(PORT, () => { 
